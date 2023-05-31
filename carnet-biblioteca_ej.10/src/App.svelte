a0db625bda291403e179f495484b3a7c8b901778
Minerva1922 <minervapv83@gmail.com>
2023-06-01 00:22:46 +0200

    whats

---
+++ carnet-biblioteca_ej.10/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+
+
+    import { onMount } from "svelte";
+    import Card from "./lib/Card.svelte";
+    let nombre = "";
+    
+  
+  onMount(() => {
+  fetch('https://random-data-api.com/api/v2/users')
+    .then(response => response.json())
+    .then(({nombre, apellidos}) => nombre = `${nombre} ${apellidos}`)
+})
+  
+</script>
+  
+  <main>
+    <Card fullUserName="Minerva Pedret Vergillos" />
+  </main>
+  
---
+++ carnet-biblioteca_ej.10/src/lib/Card.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let fullUserName;
+    let lista = fullUserName.split(" ")
+    let name = "";
+
+
+</script>
+
+<div class="card">
+    <h3>{fullUserName}</h3>
+   <p>nom: Minerva </p>
+   <p>cognoms: Pedret Vergillos</p>
+</div>
+
+<style>
+    .card {
+        background-color: #f5f5f5;
+        padding: 16px;
+        border-radius: 4px;
+        box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    }
+</style>
